0fac822907c2674771aaaf401a18d38a6929e7d6
rayoneitalo <rayoneitalo@gmail.com>
2024-03-26 07:29:01 +0000

    feat: add pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      name: 'NextAppName',
+      //   exec_mode: 'cluster',
+      instances: '2', // Or a number of instances
+      script: 'npm run start',
+      args: 'start',
+      env_local: {
+        APP_ENV: 'local', // APP_ENV=local
+      },
+      env_dev: {
+        APP_ENV: 'dev', // APP_ENV=dev
+      },
+      env_prod: {
+        APP_ENV: 'prod', // APP_ENV=prod
+      },
+    },
+  ],
+}
