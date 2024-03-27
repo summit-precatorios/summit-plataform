a43b39ed171c25786c0e754024fba0d5bd4556c9
rayoneitalo <rayoneitalo@gmail.com>
2024-03-27 11:46:04 +0000

    refactor: remove comments

--- src/components/PasswordInput.tsx
+++ src/components/PasswordInput.tsx
@@ -24,16 +24,12 @@ export default function PasswordInput(props: PasswordInputProps) {
           <Input
             size="lg"
             focusBorderColor="#737171"
-            // className="h-12 rounded-lg px-3 py-4 border-solid border-gray-200 border-[1px] focus:outline-slate-500 "
             type={show ? 'text' : 'password'}
             placeholder={props.placeholder}
             name={props.inputName}
             id={props.inputId}
           />
           <InputRightElement width="4.5rem">
-            {/* <Button h="1.75rem" size="sm" onClick={handleClick}>
-              {show ? 'Hide' : 'Show'}
-            </Button> */}
             <span onClick={handleClick} className="cursor-pointer">
               {show ? (
                 <Visibility className="w-6 h-6 text-[#64748b]" />
