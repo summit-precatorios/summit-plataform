55ff5057757bd875838a5ab42cbcf89fe068cf3c
rayoneitalo <rayoneitalo@gmail.com>
2024-04-13 14:54:46 +0000

    fix: eslint disable no-unused vars

--- src/app/(auth)/forget-password/page.tsx
+++ src/app/(auth)/forget-password/page.tsx
@@ -1,59 +1,59 @@
-// import * as z from 'zod'
+// // import * as z from 'zod'
 
-import {
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form'
-import { Input } from '@/components/ui/input'
-import { zodResolver } from '@hookform/resolvers/zod'
+// import {
+//   FormControl,
+//   FormField,
+//   FormItem,
+//   FormLabel,
+//   FormMessage,
+// } from '@/components/ui/form'
+// import { Input } from '@/components/ui/input'
+// import { zodResolver } from '@hookform/resolvers/zod'
 
-import { Form, useForm } from 'react-hook-form'
-import { z } from 'zod'
+// import { Form, useForm } from 'react-hook-form'
+// import { z } from 'zod'
 
-// TODO handleSubmit function
+// // TODO handleSubmit function
 
-// ! Magic link for recovery your account
+// // ! Magic link for recovery your account
 
-const formSchema = z.object({
-  email: z.string().email('Insira um endereço de e-mail válido.'),
-})
+// const formSchema = z.object({
+//   email: z.string().email('Insira um endereço de e-mail válido.'),
+// })
 
-export default function ForgetPassword() {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      email: '',
-    },
-  })
+// export default function ForgetPassword() {
+//   const form = useForm<z.infer<typeof formSchema>>({
+//     resolver: zodResolver(formSchema),
+//     defaultValues: {
+//       email: '',
+//     },
+//   })
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  async function onSubmit(data: z.infer<typeof formSchema>) {
-    console.log(navigator.userAgent)
-  }
+//   // eslint-disable-next-line @typescript-eslint/no-unused-vars
+//   async function onSubmit(data: z.infer<typeof formSchema>) {
+//     console.log(navigator.userAgent)
+//   }
 
-  return (
-    <div className="w-96 m-auto">
-      <h1 className="text-3xl font-semibold mb-4">Recuperar conta</h1>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>CPF</FormLabel>
-                <FormControl>
-                  <Input placeholder="exemplo@gmail.com" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </form>
-      </Form>
-    </div>
-  )
-}
+//   return (
+//     <div className="w-96 m-auto">
+//       <h1 className="text-3xl font-semibold mb-4">Recuperar conta</h1>
+//       <Form {...form}>
+//         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+//           <FormField
+//             control={form.control}
+//             name="email"
+//             render={({ field }) => (
+//               <FormItem>
+//                 <FormLabel>CPF</FormLabel>
+//                 <FormControl>
+//                   <Input placeholder="exemplo@gmail.com" {...field} />
+//                 </FormControl>
+//                 <FormMessage />
+//               </FormItem>
+//             )}
+//           />
+//         </form>
+//       </Form>
+//     </div>
+//   )
+// }
