9be9c1432b8df930da036a2d1bf6cc2da63c9351
rayoneitalo <rayoneitalo@gmail.com>
2024-04-16 19:21:30 +0000

    refactor: auth service

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -7,27 +7,13 @@ type SignInRequestData = {
 
 export async function signInRequest(data: SignInRequestData) {
   try {
-    const response = await api('signin', {
+    return await api('signin', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(data, null, 2),
     })
-
-    const { accessToken } = await response.json()
-
-    return accessToken
-    // if (!accessToken) {
-    //   toast({
-    //     variant: 'destructive',
-    //     title: 'Falha de Autenticação',
-    //     description: 'Credenciais de acesso inválidas ou não registradas',
-    //   })
-    // }
-  } catch (error) {
-    // toast({
-    //   variant: 'destructive',
-    //   title: 'Erro interno',
-    //   description: 'Não foi possível processar a sua requisição',
-    // })
-  }
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      .then(async (result: any) => await result.json())
+      .then((response) => response)
+  } catch (error) {}
 }
