20c5eb2a7c947daeffd2bd3378c80ec1b14a68fa
rayoneitalo <rayoneitalo@gmail.com>
2024-04-16 19:21:41 +0000

    refactor: change api service

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any
 export async function api(resource?: string, init?: RequestInit): Promise<any> {
   const URL = `http://localhost:4004/auth/${resource}`
-  const HTTP_TIMEOUT = 5000
+  const HTTP_TIMEOUT = 3000
 
   const controller = new AbortController()
   const timeoutId = setTimeout(() => controller.abort(), HTTP_TIMEOUT)
@@ -10,8 +10,7 @@ export async function api(resource?: string, init?: RequestInit): Promise<any> {
     return await fetch(URL, {
       ...init,
       signal: controller.signal,
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    }).then(async (result: any) => result)
+    })
   } catch (error) {
   } finally {
     clearTimeout(timeoutId)
