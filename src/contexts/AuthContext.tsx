60aa47b035f40e662735ee6e28ad2b5f19c63044
rayoneitalo <rayoneitalo@gmail.com>
2024-04-16 19:21:12 +0000

    feat: auth context

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,9 +1,10 @@
 'use client'
 
+import { useToast } from '@/components/ui/use-toast'
 import { signInRequest } from '@/services/auth'
-import { ReactNode, createContext, useState } from 'react'
-import { setCookie } from 'nookies'
 import { jwtDecode } from 'jwt-decode'
+import { setCookie } from 'nookies'
+import { ReactNode, createContext, useState } from 'react'
 
 interface AuthContextProps {
   children: ReactNode
@@ -29,21 +30,57 @@ type AuthContextType = {
 export const AuthContext = createContext({} as AuthContextType)
 
 export function AuthProvider({ children }: AuthContextProps) {
+  const { toast } = useToast()
   const [user, setUser] = useState<User | null>(null)
   const isAuthenticated = !!user
 
   async function signIn({ document, password }: SignInData) {
-    const token = await signInRequest({ document, password })
+    try {
+      const response = await signInRequest({ document, password })
 
-    setCookie(undefined, 'summit.token', token, {
-      maxAge: 60 * 60 * 1, // expires in 1 hour
-    })
+      if (!response) {
+        toast({
+          variant: 'destructive',
+          title: 'Erro interno',
+          description: 'Não foi possível processar a sua requisição',
+        })
+        return
+      }
 
-    const payload: User = jwtDecode(token)
+      if (response && response.statusCode === 401) {
+        toast({
+          variant: 'destructive',
+          title: 'Falha de Autenticação',
+          description: 'Credenciais de acesso inválidas ou não registradas',
+        })
 
-    console.log(payload)
+        return
+      }
 
-    setUser(payload)
+      const { accessToken: token } = response
+
+      if (!token)
+        toast({
+          variant: 'destructive',
+          title: 'Falha de Autenticação',
+          description: 'Credenciais de acesso inválidas ou não registradas',
+        })
+
+      setCookie(undefined, 'summit.token', token, {
+        maxAge: 60 * 60 * 1, // expires in 1 hour
+      })
+
+      const payload: User = jwtDecode(token as string)
+
+      setUser(payload)
+    } catch (error) {
+      console.log(error)
+      toast({
+        variant: 'destructive',
+        title: 'Erro interno',
+        description: 'Não foi possível processar a sua requisição',
+      })
+    }
   }
 
   return (
