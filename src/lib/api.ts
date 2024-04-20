8269f9a9abf2787e8c89de13b2e994622d81828f
root <root@summit>
2024-04-20 07:24:54 +0000

    refactor: change api url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any
 export async function api(resource?: string, init?: RequestInit): Promise<any> {
-  const URL = `http://localhost:4004/auth/${resource}`
+  const URL = `https://summitprecatorios.com.br/api/auth/${resource}`
   const HTTP_TIMEOUT = 3000
 
   const controller = new AbortController()
