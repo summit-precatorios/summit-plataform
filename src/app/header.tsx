5fae45a0a55d5ae4a1e075e0f0e86e542c8df069
rayoneitalo <rayoneitalo@gmail.com>
2024-04-22 15:48:36 +0000

    feat: responsive navbar

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
 import { UserNav } from '@/components/user-nav'
 import { AuthContext } from '@/contexts/AuthContext'
 
@@ -11,32 +12,94 @@ import { useContext } from 'react'
 export function Header() {
   const { isAuthenticated } = useContext(AuthContext)
   return (
-    <div className="relative z-10 border-b py-4 bg-gray-50">
-      <div className="items-center container mx-auto justify-between flex">
-        <div className="flex gap-12 items-center">
-          <Link href="/" className="flex gap-2 items-center text-xl text-black">
+    <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6">
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button className="lg:hidden" size="icon" variant="outline">
+            <Image src="/logo.svg" width="50" height="50" alt="summit logo" />
+            <span className="sr-only">Toggle navigation menu</span>
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left">
+          <Link className="mr-6 hidden lg:flex" href="#">
             <Image src="/logo.svg" width="100" height="100" alt="summit logo" />
+            <span className="sr-only">Summit</span>
           </Link>
-          <Link href="/dashboard">Dashboard</Link>
-        </div>
-        <div className="flex gap-12 items-center">
-          {/* <Link href="/privacy">Aviso de Privacidade</Link> */}
-          <Link href="#">Aviso de Privacidade</Link>
-          {/* <Link href="/terms-of-service">Termos de Serviço</Link> */}
-          <Link href="#">Termos de Serviço</Link>
-          {/* <Link href="/about">Sobre</Link> */}
-          <Link href="#">Sobre</Link>
-          {/* <Link href="/contact">Contato</Link> */}
-          <Link href="#">Contato</Link>
-          {!isAuthenticated ? (
-            <Button type="submit" className="h-11" asChild>
-              <Link href="/signin">Entrar</Link>
-            </Button>
-          ) : (
-            <UserNav />
-          )}
-        </div>
-      </div>
-    </div>
+          <div className="grid gap-2 py-6">
+            <Link
+              className="flex w-full items-center py-2 text-lg font-semibold"
+              href="#"
+            >
+              Aviso de Privacidade
+            </Link>
+            <Link
+              className="flex w-full items-center py-2 text-lg font-semibold"
+              href="#"
+            >
+              Termos de Serviço
+            </Link>
+            <Link
+              className="flex w-full items-center py-2 text-lg font-semibold"
+              href="#"
+            >
+              Sobre
+            </Link>
+            <Link
+              className="flex w-full items-center py-2 text-lg font-semibold"
+              href="#"
+            >
+              Contato
+            </Link>
+            {!isAuthenticated ? (
+              <Button type="submit" className="h-11" asChild>
+                <Link href="/signin">Entrar</Link>
+              </Button>
+            ) : (
+              <UserNav />
+            )}
+          </div>
+        </SheetContent>
+      </Sheet>
+      <Link className="mr-6 hidden lg:flex" href="/">
+        <Image src="/logo.svg" width="100" height="100" alt="summit logo" />
+        <span className="sr-only">Summit</span>
+      </Link>
+      <nav className="ml-auto hidden lg:flex gap-6">
+        <Link
+          className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+          href="#"
+        >
+          Aviso de Privacidade
+        </Link>
+        <Link
+          className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+          href="#"
+        >
+          Termos de Serviço
+        </Link>
+        <Link
+          className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+          href="#"
+        >
+          Sobre
+        </Link>
+        <Link
+          className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+          href="#"
+        >
+          Contato
+        </Link>
+        {!isAuthenticated ? (
+          <Link
+            className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+            href="/signin"
+          >
+            Entrar
+          </Link>
+        ) : (
+          <UserNav />
+        )}
+      </nav>
+    </header>
   )
 }
