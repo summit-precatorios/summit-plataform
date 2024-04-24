4201ec7d5e000152a8fef8661aca879face5132e
rayoneitalo <rayoneitalo@gmail.com>
2024-04-24 13:06:01 +0000

    fix: hidden dashboard link with user not authorized

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -25,6 +25,15 @@ export function Header() {
             <Image src="/logo.svg" width="100" height="100" alt="summit logo" />
             <span className="sr-only">Summit</span>
           </Link>
+          {!isAuthenticated ? null : (
+            <Link
+              className="flex w-full items-center py-2 text-lg font-semibold"
+              href="/dashboard"
+            >
+              Dashboard
+            </Link>
+          )}
+
           <div className="grid gap-2 py-6">
             <Link
               className="flex w-full items-center py-2 text-lg font-semibold"
@@ -60,10 +69,21 @@ export function Header() {
           </div>
         </SheetContent>
       </Sheet>
+
       <Link className="mr-6 hidden lg:flex" href="/">
         <Image src="/logo.svg" width="100" height="100" alt="summit logo" />
         <span className="sr-only">Summit</span>
       </Link>
+
+      {!isAuthenticated ? null : (
+        <Link
+          className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50 max-lg:hidden"
+          href="/dashboard"
+        >
+          Dashboard
+        </Link>
+      )}
+
       <nav className="ml-auto hidden lg:flex gap-6">
         <Link
           className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
