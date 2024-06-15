566ce46e5286e6f5e35fd9cfb635a3f7e68db9c9
rayoneitalo <rayoneitalo@gmail.com>
2024-06-15 02:33:17 +0000

    refactor: identify stagin environment

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -13,130 +13,141 @@ import { useContext } from 'react'
 export function Header() {
   const { isAuthenticated, user } = useContext(AuthContext)
   return (
-    <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6">
-      <Sheet>
-        <SheetTrigger asChild>
-          <Button className="lg:hidden" size="icon" variant="outline">
-            <MenuIcon className="h-6 w-6" />
-          </Button>
-        </SheetTrigger>
-        <SheetContent side="left">
-          <Link className="mr-6 hidden lg:flex" href="/">
-            <Image src="/logo.svg" width="100" height="100" alt="summit logo" />
-            <span className="sr-only">Summit</span>
-          </Link>
-          {!isAuthenticated ? null : (
-            <Link
-              className="flex w-full items-center py-2 text-lg font-semibold"
-              href="/dashboard"
-            >
-              Dashboard
+    <>
+      <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6">
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button className="lg:hidden" size="icon" variant="outline">
+              <MenuIcon className="h-6 w-6" />
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left">
+            <Link className="mr-6 hidden lg:flex" href="/">
+              <Image
+                src="/logo.svg"
+                width="100"
+                height="100"
+                alt="summit logo"
+              />
+              <span className="sr-only">Summit</span>
             </Link>
-          )}
-
-          <div className="grid gap-2 py-6">
-            <Link
-              className="flex w-full items-center py-2 text-lg font-semibold"
-              href="#"
-            >
-              Aviso de Privacidade
-            </Link>
-            <Link
-              className="flex w-full items-center py-2 text-lg font-semibold"
-              href="#"
-            >
-              Termos de Serviço
-            </Link>
-            <Link
-              className="flex w-full items-center py-2 text-lg font-semibold"
-              href="#"
-            >
-              Sobre
-            </Link>
-            <Link
-              className="flex w-full items-center py-2 text-lg font-semibold"
-              href="#"
-            >
-              Contato
-            </Link>
-            {!isAuthenticated ? (
-              <Button variant={'default'} asChild>
-                <Link href="/signin">Entrar</Link>
-              </Button>
-            ) : (
-              <UserNav />
+            {!isAuthenticated ? null : (
+              <Link
+                className="flex w-full items-center py-2 text-lg font-semibold"
+                href="/dashboard"
+              >
+                Dashboard
+              </Link>
             )}
-          </div>
-        </SheetContent>
-      </Sheet>
 
-      <Link className="mr-6 hidden lg:flex" href="/">
-        <Image src="/logo.svg" width="100" height="100" alt="summit logo" />
-        <span className="sr-only">Summit</span>
-      </Link>
-
-      {!isAuthenticated ? null : (
-        <Link
-          className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50 max-lg:hidden"
-          href="/dashboard"
-        >
-          Precatórios
-        </Link>
-      )}
-
-      <nav className="ml-auto hidden lg:flex gap-6">
-        <Link
-          className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-          href="#"
-        >
-          Aviso de Privacidade
-        </Link>
-        <Link
-          className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-          href="#"
-        >
-          Termos de Serviço
-        </Link>
-        <Link
-          className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-          href="#"
-        >
-          Sobre
-        </Link>
-        <Link
-          className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-          href="#"
-        >
-          Contato
-        </Link>
-
-        {!isAuthenticated ? (
-          <Button variant={'default'} asChild>
-            <Link
-              className=" bg-[#EAAC2E] group inline-flex h-9 w-max items-center justify-center rounded-md  px-4 py-2 text-sm font-medium transition-colors hover:opacity-80 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-              href="/signin"
-            >
-              Entrar
-            </Link>
-          </Button>
-        ) : (
-          <>
-            <div className="flex items-center gap-4">
-              <div className="relative">
-                <Button size="icon" variant="ghost">
-                  <BellIcon className="h-5 w-5" />
-                  <span className="sr-only">Notifications</span>
+            <div className="grid gap-2 py-6">
+              <Link
+                className="flex w-full items-center py-2 text-lg font-semibold"
+                href="#"
+              >
+                Aviso de Privacidade
+              </Link>
+              <Link
+                className="flex w-full items-center py-2 text-lg font-semibold"
+                href="#"
+              >
+                Termos de Serviço
+              </Link>
+              <Link
+                className="flex w-full items-center py-2 text-lg font-semibold"
+                href="#"
+              >
+                Sobre
+              </Link>
+              <Link
+                className="flex w-full items-center py-2 text-lg font-semibold"
+                href="#"
+              >
+                Contato
+              </Link>
+              {!isAuthenticated ? (
+                <Button variant={'default'} asChild>
+                  <Link href="/signin">Entrar</Link>
                 </Button>
-                {!user?.isActive ? (
-                  <span className="absolute top-0 right-0 w-2 h-2 bg-red-500 rounded-full" />
-                ) : null}
-              </div>
+              ) : (
+                <UserNav />
+              )}
             </div>
-            <UserNav />
-          </>
+          </SheetContent>
+        </Sheet>
+
+        <Link className="mr-6 hidden lg:flex" href="/">
+          <Image src="/logo.svg" width="100" height="100" alt="summit logo" />
+          <span className="sr-only">Summit</span>
+        </Link>
+
+        {!isAuthenticated ? null : (
+          <Link
+            className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50 max-lg:hidden"
+            href="/dashboard"
+          >
+            Precatórios
+          </Link>
         )}
-      </nav>
-    </header>
+
+        <nav className="ml-auto hidden lg:flex gap-6">
+          <Link
+            className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+            href="#"
+          >
+            Aviso de Privacidade
+          </Link>
+          <Link
+            className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+            href="#"
+          >
+            Termos de Serviço
+          </Link>
+          <Link
+            className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+            href="#"
+          >
+            Sobre
+          </Link>
+          <Link
+            className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+            href="#"
+          >
+            Contato
+          </Link>
+
+          {!isAuthenticated ? (
+            <Button variant={'default'} asChild>
+              <Link
+                className=" bg-[#EAAC2E] group inline-flex h-9 w-max items-center justify-center rounded-md  px-4 py-2 text-sm font-medium transition-colors hover:opacity-80 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+                href="/signin"
+              >
+                Entrar
+              </Link>
+            </Button>
+          ) : (
+            <>
+              <div className="flex items-center gap-4">
+                <div className="relative">
+                  <Button size="icon" variant="ghost">
+                    <BellIcon className="h-5 w-5" />
+                    <span className="sr-only">Notifications</span>
+                  </Button>
+                  {!user?.isActive ? (
+                    <span className="absolute top-0 right-0 w-2 h-2 bg-red-500 rounded-full" />
+                  ) : null}
+                </div>
+              </div>
+              <UserNav />
+            </>
+          )}
+        </nav>
+      </header>
+
+      <div className="container bg-red-400 rounded-sm p-6 flex justify-center items-center m-auto">
+        <h1 className="text-white text-2xl">Ambiente de Homologação</h1>
+      </div>
+    </>
   )
 
   function BellIcon(props: { className: string }) {
