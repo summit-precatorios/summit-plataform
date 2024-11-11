fedc2ec025758548efca5c09dfa5ac75af5897dc
rayoneitalo <rayoneitalo@gmail.com>
2024-11-11 13:06:15 +0000

    fix(roles): remove unused variables 'props', 'isAuthenticated', and 'user'

--- src/components/roles.tsx
+++ src/components/roles.tsx
@@ -1,20 +1,20 @@
-import { AuthContext } from '@/contexts/AuthContext'
-import { ReactNode, useContext } from 'react'
+// import { AuthContext } from '@/contexts/AuthContext'
+// import { ReactNode, useContext } from 'react'
 
-type Roles = 'commoun-user'
+// type Roles = 'commoun-user'
 
-interface Check {
-  condition: () => boolean
-  element: ReactNode
-}
+// interface Check {
+//   condition: () => boolean
+//   element: ReactNode
+// }
 
-interface RoleViewProps {
-  children: ReactNode
-  allow: Roles[]
-  checks?: Check[]
-  allowFetch?: () => void
-}
+// interface RoleViewProps {
+//   children: ReactNode
+//   allow: Roles[]
+//   checks?: Check[]
+//   allowFetch?: () => void
+// }
 
-export function RoleView(props: RoleViewProps) {
-  const { isAuthenticated, user } = useContext(AuthContext)
-}
+// // export function RoleView(props: RoleViewProps) {
+// //   const { isAuthenticated, user } = useContext(AuthContext)
+// // }
